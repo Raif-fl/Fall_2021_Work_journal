103082a860b4a278d9f552f2ce8b0808361d845c
Raif-fl <flanagan.keegan16@gmail.com>
2021-09-08 15:54:42 -0700

    Setting up framework to complete intro file

---
+++ Aligning_the_genome_step14_part2.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+#SBATCH --time=00:30:00
+#SBATCH --array=1-4
+#SBATCH --account=def-egreenbl
+#SBATCH --job-name=Download_SSR
+#SBATCH --output=output/%x-%j.out
+#SBATCH --mail-user=keeganfl@student.ubc.ca
+#SBATCH --mail-type=END
+#SBATCH --mail-type=BEGIN
+#SBATCH --mail-type=FAIL
+
+STAR --genomeDir $HOME/projects/def-egreenbl/egreenbl/genomes/fly \
+--runThreadN 8 \
+--readFilesIn control_RPF_${SLURM_ARRAY_TASK_ID}.norrna.fastq \
+--outFileNamePrefix control_RPF_${SLURM_ARRAY_TASK_I}_ \
+--outSAMtype BAM SortedByCoordinate \
+--outSAMunmapped Within \
+--outSAMattributes Standard
---
+++ test.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+Check=$"5"
+
+echo blah_$Check_blah
+echo blah_$Check_ blah
+echo blah_$Check._blah
+echo blah_$Check$_blah
+echo blah_${Check}_blah

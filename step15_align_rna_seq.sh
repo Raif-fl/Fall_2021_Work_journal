1782185f9e3d5615062a7a578f50e9c0ff6a7ffe
Raif-fl <flanagan.keegan16@gmail.com>
2021-09-09 13:04:06 -0700

    Adjusted the times used for the run

--- step15_align_rna_seq.sh
+++ step15_align_rna_seq.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-#SBATCH --time=00:30:00
+#SBATCH --time=02:00:00
 #SBATCH --array=1-9
 #SBATCH --account=def-egreenbl
 #SBATCH --job-name=Align_rna
--- unzip_SRR_files.sh
+++ unzip_SRR_files.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
-#SBATCH --time=01:00:00
+#SBATCH --time=02:00:00
 #SBATCH --account=def-egreenbl
+#SBATCH --mem=8G
 #SBATCH --job-name=Unzip_SSR
 #SBATCH --output=output/%x-%j.out
 #SBATCH --mail-user=keeganfl@student.ubc.ca

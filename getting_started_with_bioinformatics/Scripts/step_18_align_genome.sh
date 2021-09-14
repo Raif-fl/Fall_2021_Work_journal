3a70f88a4b757f0bb72e2c41c1190e0dbd13a3fb
Raif-fl <flanagan.keegan16@gmail.com>
2021-09-14 16:02:09 -0700

    Fixed the name of the list called in each script

--- getting_started_with_bioinformatics/Scripts/step_18_align_genome.sh
+++ getting_started_with_bioinformatics/Scripts/step_18_align_genome.sh
@@ -14,7 +14,7 @@
 # Commands you wish to run.
 
 echo "Starting task $SLURM_ARRAY_TASK_ID"
-commands=$(sed -n "${SLURM_ARRAY_TASK_ID}p" step_14_genome_align_list)
+commands=$(sed -n "${SLURM_ARRAY_TASK_ID}p" step_18_genome_align_list)
 
-# Then execute all of the commands in parrallel. 
+# Then execute all of the commands in parrallel.
 eval $commands

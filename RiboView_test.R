e7798abe16806a2571f29d6692b0b8cf8cf9a08a
Raif-fl <flanagan.keegan16@gmail.com>
2021-09-27 16:57:40 -0700

    updated_documentation

---
+++ RiboView_test.R
@@ -0,0 +1,10 @@
+library("devtools")
+
+#install.packages("~/Desktop/RiboVIEWreticulate_2.1.tar.gz", repos = NULL, type ="source")
+
+#install.packages("reticulate")
+#install.packages("Rtsne")
+
+library(RiboVIEWreticulate)
+
+help("RiboVIEWreticulate")

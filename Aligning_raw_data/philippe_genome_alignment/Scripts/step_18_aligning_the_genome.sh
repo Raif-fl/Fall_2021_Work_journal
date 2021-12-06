e4b37f9592bbaa5abce5f3105b35bd2ee235d72f
Keegan <flanagan.keegan16@gmail.com>
2021-12-06 11:14:00 -0800

    Split KS notebook into three for ecoli

--- Aligning_raw_data/philippe_genome_alignment/Scripts/step_18_aligning_the_genome.sh
+++ Aligning_raw_data/philippe_genome_alignment/Scripts/step_18_aligning_the_genome.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
-#SBATCH --time=00:30:00
+#SBATCH --time=01:00:00
 #SBATCH --array=1-8
 #SBATCH --cpus-per-task=9
-#SBATCH --mem=32G
+#SBATCH --mem=200G
 #SBATCH --account=def-egreenbl
 #SBATCH --job-name=align_genome
 #SBATCH --output=output/%x-%j.out

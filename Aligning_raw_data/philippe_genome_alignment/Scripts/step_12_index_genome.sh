31603a6dcb1e98c6e486251da53841d982806675
Keegan <flanagan.keegan16@gmail.com>
2021-12-06 10:02:36 -0800

    Created scripts for alligning data from villar and philippe papers

---
+++ Aligning_raw_data/philippe_genome_alignment/Scripts/step_12_index_genome.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+#SBATCH --time=03:00:00
+#SBATCH --mem=200G
+#SBATCH --cpus-per-task=10
+#SBATCH --account=def-egreenbl
+#SBATCH --job-name=Index_genome
+#SBATCH --output=%x-%j.out
+#SBATCH --mail-user=keeganfl@student.ubc.ca
+#SBATCH --mail-type=END
+#SBATCH --mail-type=BEGIN
+#SBATCH --mail-type=FAIL
+
+export OMP_NUM_THREADS=$SLURM_CPUS_PER_TASK
+
+STAR --runThreadN 8 --limitGenomeGenerateRAM 20000000000 --runMode genomeGenerate --genomeDir . --genomeFastaFiles hg38.fa.masked --sjdbGTFfile hg38.refGene.gtf  --sjdbOverhang 100 --genomeSAindexNbases 13
